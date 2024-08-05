58dfcff4a2b27c518a2b8f5158b6068c0ae19fc8
JohanUwUTzul <johanisraeltzulcoyoy@gmail.com>
2024-08-05 00:38:52 -0600

    Add files via upload

---
+++ unitesting/src/test/java/NotificadorEmailTest.java
@@ -0,0 +1,31 @@
+import static org.mockito.Mockito.*;
+import org.example.EmailCliente;
+import org.example.NotificadorEmail;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+@ExtendWith(MockitoExtension.class)
+public class NotificadorEmailTest {
+    @Mock
+    private EmailCliente emailClienteMock;
+    @Test
+    public void testNotificar() {
+        NotificadorEmail notificador = new NotificadorEmail(emailClienteMock);
+        notificador.notificar("ejemplo@test.com", "Hola Mundo");
+        verify(emailClienteMock).enviarCorreo("ejemplo@test.com", "hola Guatemala");}
+    @Test
+    public void testNotificarConDireccionVacia() {
+        NotificadorEmail notificador = new NotificadorEmail(emailClienteMock);
+        notificador.notificar("", "not founddawhdba");
+        verify(emailClienteMock, times(0)).enviarCorreo(anyString(), anyString());}
+    @Test
+    public void testNotificarConMensajeNulo() {
+        NotificadorEmail notificador = new NotificadorEmail(emailClienteMock);
+        notificador.notificar("ejemplo@test.com", null);
+        verify(emailClienteMock).enviarCorreo("ejemplo@test.com", null);}
+    @Test
+    public void testNotificarConDireccionNula(){
+        NotificadorEmail notificador=new NotificadorEmail(emailClienteMock);
+        notificador.notificar(null,"no se encontro ");
+        verify(emailClienteMock,times(0)).enviarCorreo(anyString(),anyString());}}
